4da7f05196f78bb0605f2c30378ea1554237ae0d
Abigail Vania <abigailvaniapra@gmail.com>
2024-10-22 11:48:32 +0700

    adding forgot password

---
+++ forgot_password.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forgot Password</title>
+    <link rel="stylesheet" href="style_user_regist.css">
+</head>
+
+<body>
+    <div class="forgot-password-container">
+        <h2>Forgot Password</h2>
+        <h6>Don't worry! We got your back!</h6>
+    </div>
+    <div class="forgot-password-form">
+        <form action="send_otp.php">
+            
+        </form>
+    </div>
+</body>
+
+</html>
